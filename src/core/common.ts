f8df58566b1c3810d2bcd6907a5a7f811bb0c452
Jason <wenjsen@gmail.com>
2019-09-20 17:39:05 +0800

    fix bug

--- src/core/common.ts
+++ src/core/common.ts
@@ -1,6 +1,6 @@
 import { ArgumentsError } from '../error/argumentsError';
 export const validTopicName = (name: any) => {
-  if (!name || typeof name !== 'string' || /^[a-zA-Z][a-zA-Z0-9]/.test(name)) {
+  if (!name || typeof name !== 'string' || !/^[a-zA-Z][a-zA-Z0-9_-]/.test(name)) {
     throw new ArgumentsError();
   }
 };
